43366f44a92f531ef8b2f45e1bff36f547c2cfb1
Eyob-coder <oddjobera@gmail.com>
2023-03-16 21:18:17 +0300

    50vh done, skew issue

--- src/Homepage.js
+++ src/Homepage.js
@@ -27,48 +27,77 @@ const HomePage = () => {
   };
 
   const init = () => {
-    // Create a new scene
-    const scene = new THREE.Scene();
+  // Create a new scene
+  const scene = new THREE.Scene();
 
-    // Set up camera
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.z = 5;
+  // Set up camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
 
-    // Set up renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setClearColor(0x000000);
+  // Set up renderer
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setClearColor(0x000000);
 
-    // Add canvas to DOM
-    const canvasContainer = document.getElementById("canvas-container");
-    canvasContainer.appendChild(renderer.domElement);
+  // Add canvas to DOM
+  const canvasContainer = document.getElementById("canvas-container");
+  canvasContainer.appendChild(renderer.domElement);
+  
+  // Resize canvas to fit 100vh
+  const canvas = renderer.domElement;
+  canvas.style.height = "50vh";
+  canvas.style.width = "100vw";
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
-    // Set up orbit controls
-    const controls = new OrbitControls(camera, renderer.domElement);
 
-    // Add pyramids
-    const geometry = new THREE.ConeGeometry(0.25, 1, 4);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xffffff,
-      wireframe: true,
-    });
+  // Set up orbit controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true; // Add damping for smoother mouse movement
+  controls.dampingFactor = 0.1;
+  controls.rotateSpeed = 0.5; // Adjust rotation speed
+  controls.zoomSpeed = 1; // Adjust zoom speed
 
-    for (let i = 0; i < 1000; i++) {
-      const mesh = new THREE.Mesh(geometry, material);
-      mesh.position.x = Math.random() * 10 - 5;
-      mesh.position.y = Math.random() * 10 - 5;
-      mesh.position.z = Math.random() * 10 - 5;
-      mesh.rotation.x = Math.random() * 2 * Math.PI;
-      mesh.rotation.y = Math.random() * 2 * Math.PI;
-      mesh.rotation.z = Math.random() * 2 * Math.PI;
-      scene.add(mesh);
-    }
+  // Add pyramids
+  const geometry = new THREE.ConeGeometry(0.25, 1, 4);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true,
+  });
+
+  for (let i = 0; i < 1000; i++) {
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = Math.random() * 10 - 5;
+    mesh.position.y = Math.random() * 10 - 5;
+    mesh.position.z = Math.random() * 10 - 5;
+    mesh.rotation.x = Math.random() * 2 * Math.PI;
+    mesh.rotation.y = Math.random() * 2 * Math.PI;
+    mesh.rotation.z = Math.random() * 2 * Math.PI;
+    scene.add(mesh);
+  }
+
+  // Add mouse movement interaction
+  const onMouseMove = (event) => {
+    const mouseX = event.clientX / window.innerWidth - 0.5;
+    const mouseY = event.clientY / window.innerHeight - 0.5;
+    camera.position.x = mouseX * 10;
+    camera.position.y = -mouseY * 10;
+    camera.lookAt(scene.position);
+  };
+  document.addEventListener("mousemove", onMouseMove, false);
+
+  // Add mouse wheel zoom interaction
+  const onMouseWheel = (event) => {
+    const zoomSpeed = 0.2;
+    camera.position.z += event.deltaY * zoomSpeed;
+  };
+  document.addEventListener("wheel", onMouseWheel, false);
+
 
     // Animate pyramids
     const animate = () => {
